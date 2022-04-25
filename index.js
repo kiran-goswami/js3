48d6c7d461d199695dc0220724b41eb9cc4b911f
kiran-goswami <kiran191541535@gmail.com>
2022-04-25 20:50:27 +0530

    v1

---
+++ index.js
@@ -0,0 +1,67 @@
+console.log("working");
+// arithematic operator
+// +,-,*,/,%
+// modulus - it gives us remainder
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2/3);
+console.log(2%3);
+// comaprision operators
+// <,>,<=,>=,==,===
+// in comparision true false value we get
+console.log(2<3); 
+// true(only boolean values)
+console.log(2<=3);
+// we will take it as or operator and true
+console.log(2==2);
+// true
+console.log(2===2);
+// true
+console.log(2=="2");
+// true
+console.log(2==="2");
+// false
+// == only checks the value but not data type
+// ===checks both the values and the data types
+console.log(2!=3);
+// !=,!==
+// logical operators
+// ||,&&,!
+console.log(2||3);
+// one value will get print and if the  first value
+// non -zero then first will be output if it is zero then it will move to next one
+console.log(0||3);
+console.log(0||0); 
+// it will first check first value it will check the value non zero or zero it 
+console.log(0&&3);
+// 0 answer
+console.log(1&&3);
+// 3 last value will get print i.e 3
+console.log(1&&0);
+// 0
+console.log(!0);
+// true
+console.log(!1);
+// false
+console.log(!2);
+// false
+// increment decrement operators
+let a=1;
+a++;
+console.log(a);
+a--;
+// console.log(a++);
+// console.log(a++);//1//i will first print the value of a, and then increment its value with 1
+//  which will be reflected/shown in next line,not on the same line
+console.log(a--);
+console.log(a);
+let b=2;
+b*=3;
+// b=b*3;
+b/=3;
+// b=b/3;
+b+=2;
+// b=b+2;
+
+
